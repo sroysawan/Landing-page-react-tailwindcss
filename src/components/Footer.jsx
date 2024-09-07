1c68adf84a32589bac3fa02b78a483fc4e6993f8
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2024-09-07 11:32:42 +0700

    style: add footer page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 
 function Footer() {
   return (
-    <footer className="bg-blue-500 text-white py-4">
+    <footer className="bg-blue-500 text-white pt-8 pb-6">
         <div className="container mx-auto text-center">
             <p>&copy; 2024 ReactSite. All Rights Reserved</p>
         </div>
